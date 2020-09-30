e1bd0163837812b644e1a1569ba5e231f3dbd5e9
baclemen <clemens.s.bachmann@gmail.com>
2020-09-30 22:19:31 +0200

    Updates

---
+++ precache-manifest.a99acc9e11e980e07905172a4617ad53.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b5603d1e7cf8453be669bce37a9d21",
+    "url": "/cb_pen_planeapp/index.html"
+  },
+  {
+    "revision": "32f21a77a2c53aa2b76c",
+    "url": "/cb_pen_planeapp/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "c87975d477532b9ae14b",
+    "url": "/cb_pen_planeapp/static/css/main.160146bd.chunk.css"
+  },
+  {
+    "revision": "32f21a77a2c53aa2b76c",
+    "url": "/cb_pen_planeapp/static/js/2.ce7ce8a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cb_pen_planeapp/static/js/2.ce7ce8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87975d477532b9ae14b",
+    "url": "/cb_pen_planeapp/static/js/main.fd86194e.chunk.js"
+  },
+  {
+    "revision": "7207bfd1571f9276fcd5",
+    "url": "/cb_pen_planeapp/static/js/runtime-main.650c243a.js"
+  },
+  {
+    "revision": "b97243dddc400572ddc89f5425134251",
+    "url": "/cb_pen_planeapp/static/media/destfrom.b97243dd.svg"
+  },
+  {
+    "revision": "e231c83a9ac5a424c2634ac0d2e8decc",
+    "url": "/cb_pen_planeapp/static/media/destto.e231c83a.svg"
+  },
+  {
+    "revision": "46cbc93e4fed87419fbcea04976fcc4f",
+    "url": "/cb_pen_planeapp/static/media/noun_Suitcase_10620.46cbc93e.svg"
+  },
+  {
+    "revision": "770947e6a4e6ef1eb4765374bd4eaa61",
+    "url": "/cb_pen_planeapp/static/media/noun_percentage_108380.770947e6.svg"
+  },
+  {
+    "revision": "5f6c03a0a060fbd006b68431a532e2ea",
+    "url": "/cb_pen_planeapp/static/media/penflight logo.5f6c03a0.svg"
+  }
+]);
